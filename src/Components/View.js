be3abad62097eaf67176cb19bc0d696451b482aa
Arraidh <ari.darmika@gmail.com>
2021-09-26 17:52:29 +0800

    adding view

---
+++ src/Components/View.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const View = () => {
+    return (
+        <div>
+            <div class="bg-gray-800 flex justify-around w-full min-hmax-h-96 overflow-auto" style="min-height: 20em;">
+                <div class="m-8" style="max-width:30%;"></div>
+                <div class="m-8" style="max-width:30%;"></div>
+                <div class="m-8" style="max-width:30%;"></div>
+            </div>
+
+            <div class="flex justify-center flex-wrap content-center mt-32  text-6xl uppercase font-light tracking-widest text-center">the United Kingdom of Great Britain and Northern Ireland</div>
+        </div>
+    )
+}
+
+export default View
